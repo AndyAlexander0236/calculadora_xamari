0bd6ec096e632d4f5daa24afb89a79b62a83704d
Andy Alexander Logacho <logachologacho@gmail.com>
2024-06-04 08:51:52 -0500

    Agregar archivos de proyecto.

---
+++ Tarea1/Tarea1/MainPage.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using static Tarea1.Registro;
+
+namespace Tarea1
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<Registro.Operacion> Operaciones { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Operaciones = new ObservableCollection<Registro.Operacion>();
+            OperationsListView.ItemsSource = Operaciones;
+        }
+
+        private void OnOperationButtonClicked(object sender, EventArgs e)
+        {
+            if (int.TryParse(FirstNumberEntry.Text, out int firstNumber) && int.TryParse(SecondNumberEntry.Text, out int secondNumber))
+            {
+                string operacion = (string)((Button)sender).CommandParameter;
+                double resultado = 0;
+
+                switch (operacion)
+                {
+                    case "Sumar":
+                        resultado = firstNumber + secondNumber;
+                        break;
+                    case "Restar":
+                        resultado = firstNumber - secondNumber;
+                        break;
+                    case "Multiplicar":
+                        resultado = firstNumber * secondNumber;
+                        break;
+                    case "Dividir":
+                        if (secondNumber != 0)
+                        {
+                            resultado = (double)firstNumber / secondNumber;
+                        }
+                        else
+                        {
+                            ResultLabel.Text = "No se puede dividir por cero.";
+                            return;
+                        }
+                        break;
+                }
+
+                // Registrar la operación
+                Operaciones.Add(new Registro.Operacion
+                {
+                    Fecha = DateTime.Now,
+                    TipoOperacion = operacion,
+                    Numero1 = firstNumber,
+                    Numero2 = secondNumber,
+                    Resultado = resultado
+                });
+
+                // Mostrar el resultado
+                ResultLabel.Text = $"El resultado es: {resultado}";
+            }
+            else
+            {
+                ResultLabel.Text = "Por favor, ingrese números válidos.";
+            }
+        }
+
+        private void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            var operacion = button?.CommandParameter as Registro.Operacion;
+
+            if (operacion != null)
+            {
+                Operaciones.Remove(operacion);
+            }
+        }
+
+        private async void OnVerRegistroButtonClicked(object sender, EventArgs e)
+        {
+            // Crear una instancia de la página Registro y pasar la colección de operaciones
+            Registro registro = new Registro(Operaciones);
+
+            // Navegar a la página Registro
+            await Navigation.PushAsync(registro);
+        }
+
+        private async void OnGoToRegistroButtonClicked(object sender, EventArgs e)
+        {
+            // Crear una instancia de la página Registro y pasar la colección de operaciones
+            Registro registro = new Registro(Operaciones);
+
+            // Navegar a la página Registro
+            await Navigation.PushAsync(registro);
+        }
+
+
+
+    }
+}
---
+++ Tarea1/Tarea1/Registro.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tarea1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        private ObservableCollection<Operacion> _operaciones;
+
+        public Registro(ObservableCollection<Operacion> operaciones)
+        {
+            InitializeComponent();
+            _operaciones = operaciones;
+            OperationsListView.ItemsSource = _operaciones;
+        }
+
+        private async void OnDeleteClicked(object sender, EventArgs e)
+        {
+            var menuItem = sender as MenuItem;
+            var operacion = menuItem.CommandParameter as Operacion;
+
+            if (operacion != null)
+            {
+                bool answer = await DisplayAlert("Confirmación", "¿Estás seguro de eliminar este registro?", "Sí", "Cancelar");
+                if (answer)
+                {
+                    if (_operaciones.Contains(operacion))
+                    {
+                        _operaciones.Remove(operacion);
+                    }
+                }
+            }
+        }
+
+        public class Operacion
+        {
+            public DateTime Fecha { get; set; }
+            public string TipoOperacion { get; set; }
+            public int Numero1 { get; set; }
+            public int Numero2 { get; set; }
+            public double Resultado { get; set; }
+        }
+    }
+}
